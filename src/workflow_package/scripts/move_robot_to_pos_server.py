38b1f3aaa013cf2552e28fbd3a0a9fc4a92a2aae
LukasBrenneisen <luk.brenneisen@web.de>
2025-10-31 10:52:55 +0100

    Update nach Neuinstallation

---
+++ src/workflow_package/scripts/move_robot_to_pos_server.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from workflow_package.srv import MoveTheRobotToPos as MoveTheRobotToPos
+
+class MoveTheRobotToPosServer(Node):
+    de
+    
